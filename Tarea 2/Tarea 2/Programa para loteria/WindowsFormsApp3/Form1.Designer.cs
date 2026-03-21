82c64bf1132d4d587ad3f255615d2d472b7de390
Sefora008 <mojicamelendezs@gmail.com>
2026-03-20 22:44:05 -0400

    Tarea 2
    Tarea 2

---
+++ Tarea 2/Tarea 2/Programa para loteria/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lstHistory = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnJugar
+            // 
+            this.btnJugar.Location = new System.Drawing.Point(12, 12);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(100, 40);
+            this.btnJugar.TabIndex = 0;
+            this.btnJugar.Text = "Jugar";
+            this.btnJugar.UseVisualStyleBackColor = true;
+            this.btnJugar.Click += new System.EventHandler(this.btnJugar_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(12, 65);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 20);
+            this.lblResult.TabIndex = 1;
+            // 
+            // lstHistory
+            // 
+            this.lstHistory.FormattingEnabled = true;
+            this.lstHistory.ItemHeight = 16;
+            this.lstHistory.Location = new System.Drawing.Point(12, 100);
+            this.lstHistory.Name = "lstHistory";
+            this.lstHistory.Size = new System.Drawing.Size(460, 260);
+            this.lstHistory.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 381);
+            this.Controls.Add(this.lstHistory);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnJugar);
+            this.Name = "Form1";
+            this.Text = "Lotería - Tarea 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.ListBox lstHistory;
+    }
+}
+
---
+++ Tarea 2/Tarea 2/Programa para loteria/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        // File-based simple database
+        private readonly string dbFile = Path.Combine(Application.StartupPath, "numeros.xml");
+        private List<Numero> db = new List<Numero>();
+        private Random rng = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDatabase();
+            RefreshHistory();
+        }
+
+        private void btnJugar_Click(object sender, EventArgs e)
+        {
+            // Generate three random numbers 1-100
+            int primera = rng.Next(1, 101);
+            int segunda = rng.Next(1, 101);
+            int tercera = rng.Next(1, 101);
+
+            int nextId = db.Any() ? db.Max(x => x.Id) + 1 : 1;
+
+            var numero = new Numero
+            {
+                Id = nextId,
+                Primera = primera,
+                Segunda = segunda,
+                Tercera = tercera,
+                FechaHora = DateTime.Now
+            };
+
+            // Save to file-based "database"
+            db.Add(numero);
+            SaveDatabase();
+
+            // Display current result
+            lblResult.Text = $"ID {numero.Id} - {numero.FechaHora:G} - {numero.Primera}, {numero.Segunda}, {numero.Tercera}";
+
+            // Update history list
+            RefreshHistory();
+        }
+
+        private void RefreshHistory()
+        {
+            lstHistory.Items.Clear();
+            foreach (var n in db.OrderByDescending(x => x.FechaHora))
+            {
+                lstHistory.Items.Add($"ID {n.Id} - {n.FechaHora:G} - {n.Primera}, {n.Segunda}, {n.Tercera}");
+            }
+        }
+
+        private void LoadDatabase()
+        {
+            try
+            {
+                if (File.Exists(dbFile))
+                {
+                    var ser = new XmlSerializer(typeof(List<Numero>));
+                    using (var fs = File.OpenRead(dbFile))
+                    {
+                        var obj = ser.Deserialize(fs) as List<Numero>;
+                        if (obj != null)
+                            db = obj;
+                    }
+                }
+                else
+                {
+                    db = new List<Numero>();
+                }
+            }
+            catch
+            {
+                // If load fails, start with empty DB
+                db = new List<Numero>();
+            }
+        }
+
+        private void SaveDatabase()
+        {
+            try
+            {
+                var ser = new XmlSerializer(typeof(List<Numero>));
+                using (var fs = File.Create(dbFile))
+                {
+                    ser.Serialize(fs, db);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving database: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class Numero
+    {
+        public int Id { get; set; }
+        public int Primera { get; set; }
+        public int Segunda { get; set; }
+        public int Tercera { get; set; }
+        public DateTime FechaHora { get; set; }
+
+        // Parameterless constructor required for XML serialization
+        public Numero() { }
+    }
+}
